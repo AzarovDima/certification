8b9e1a05cc7335656af6698c0eeafdb902e302c5
AzarovDima <ada89885308140@gmail.com>
2023-02-19 14:37:08 +0300

    Написал код задачи

--- Program.cs
+++ Program.cs
@@ -5,4 +5,31 @@
 // При решении не рекомендуется пользоваться коллекциями, лучшеstring[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 
 
+string[] array1 = new string[4] { "hello", "22", "world", ":)" };
+string[] array2 = new string[array1.Length];
+SecondArray(array1, array2);
+PrintArray(array2);
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int newSize = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[newSize] = array1[i];
+            newSize++;
+        }
+    }
+}
+void PrintArray(string[] array2)
+{
+    Console.Write("[");
+    for (int i = 0; i < array2.Length; i++)
+    {
+        if (i < array2.Length) Console.Write($"{array2[i]}, "); // Не получается убратть линшние запятые...
+        else Console.Write($"");
+    }
+    Console.Write("]");
+}
 
